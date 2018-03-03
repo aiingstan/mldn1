d99f223d61e761129ff31ce3fd7b788317a4bd8e
yufeng <guo_yufeng@outlook.com>
2018-03-03 08:48:27 +0800

    round 2

--- p2/media.py
+++ p2/media.py
@@ -2,7 +2,16 @@
 
 
 class Movie():
+    """
+    Movie.
+    """
     def __init__(self, title, story_line, poster_image_url, trailer_url):
+        """ Initialize a movie instance.
+        :param title: movie title
+        :param story_line: movie story line
+        :param poster_image_url: url of movie poster
+        :param trainler_url: url of movie trailer
+        """
         self.title = title
         self.story_line = story_line
         self.poster_image_url = poster_image_url
